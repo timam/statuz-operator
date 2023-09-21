4bdc322af8b4410f39e9c61a9f5bb08a5d7a4bd8
Timam <mail@timam.io>
2023-09-22 02:02:43 +0600

    using timam.io as domain

--- main.go
+++ main.go
@@ -67,7 +67,7 @@ func main() {
 		Port:                   9443,
 		HealthProbeBindAddress: probeAddr,
 		LeaderElection:         enableLeaderElection,
-		LeaderElectionID:       "49c35ce5.statuz.timam.io",
+		LeaderElectionID:       "49c35ce5.timam.io",
 		// LeaderElectionReleaseOnCancel defines if the leader should step down voluntarily
 		// when the Manager ends. This requires the binary to immediately end when the
 		// Manager is stopped, otherwise, this setting is unsafe. Setting this significantly
